98730f74c769f3372e0deaf5c3ed24c3a6cad4f9
Noey <noeyislearning@gmail.com>
2024-06-02 23:25:03 +0800

    refactor: change `nextra` to `starlight` framework

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
+
+import sidebar from "./src/config/sidebar"
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      logo: {
+        src: "./src/assets/img/logo.png",
+        alt: "Lectures Logo",
+        replacesTitle: true,
+      },
+      title: "Lectures",
+      social: {
+        github: "https://github.com/cogxen/lectures",
+      },
+      customCss: [
+        "@fontsource/space-mono/400.css",
+        "@fontsource/space-mono/700.css",
+        "./src/styles/custom.css",
+      ],
+      editLink: {
+        baseUrl: "https://github.com/cogxen/lectures/issues/new",
+      },
+      lastUpdated: true,
+      sidebar: sidebar,
+    }),
+  ],
+})
--- docs/.vitepress/config/nav/index.ts
+++
@@ -1,5 +0,0 @@
-export default [
-  { text: "Home", link: "/" },
-  { text: "Datasets", link: "/datasets/" },
-  { text: "Projects", link: "/projects/" },
-]
