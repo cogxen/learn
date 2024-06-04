432da184174038f3cfdc092be7cf8cff20090db9
Noey <noeyislearning@gmail.com>
2024-06-05 00:11:32 +0800

    feat: add CustomBadge component and update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -9,11 +9,13 @@ import "../assets/css/tailwind.postcss"
 /** Components */
 import Card from "../components/Card.vue"
 import CustomLayout from "../components/CustomLayout.vue"
+import CustomBadge from "../components/CustomBadge.vue"
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component("Card", Card)
+    app.component("CustomBadge", CustomBadge)
   },
   Layout: CustomLayout,
 } satisfies ThemeConfig
