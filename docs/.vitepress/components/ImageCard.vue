89df160991027c9ff113c17734a242731851ddab
Noey <noeyislearning@gmail.com>
2024-06-08 13:52:27 +0800

    refactor: update ImageCard component to support border styling and default caption

--- docs/.vitepress/components/ImageCard.vue
+++ docs/.vitepress/components/ImageCard.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="w-full h-full overflow-hidden relative">
-    <img
-      class="h-full w-full object-cover rounded-xl border border-dashed border-slate-400 dark:border-none"
-      :src="img_url"
-      :alt="caption"
-    />
-    <div
-      class="absolute bottom-2 left-2 bg-slate-700 text-white text-[10px] px-2 py-1 rounded-lg cursor-pointer"
-    >
-      {{ copyright_owner }}
+  <component>
+    <div class="w-full h-full overflow-hidden relative">
+      <img
+        class="h-full w-full object-cover rounded-xl"
+        :class="bordered ? 'border border-dashed border-slate-400 dark:border-none' : ''"
+        :src="img_url"
+        :alt="caption"
+        v-bind="$attrs"
+      />
+      <div class="absolute top-2 left-2 bg-slate-700 text-white text-[10px] px-2 py-1 rounded-lg">
+        {{ copyright_owner }}
+      </div>
     </div>
-  </div>
-  <div class="flex pt-2 flex-col items-center">
-    <div class="text-sm text-slate-400">{{ caption }}</div>
-  </div>
+    <div class="flex pt-1 flex-col items-center">
+      <div class="text-sm text-slate-400">{{ caption }}</div>
+    </div>
+  </component>
 </template>
 
 <script setup lang="ts">
 defineProps({
   caption: {
     type: String,
+    default: "Cogxen Images",
   },
   copyright_owner: {
     type: String,
@@ -28,6 +31,10 @@ defineProps({
   img_url: {
     type: String,
     default: "https://via.placeholder.com/1200",
+  },
+  bordered: {
+    type: Boolean,
+    default: false,
   },
 })
 </script>
