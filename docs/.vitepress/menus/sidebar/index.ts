be420a70a0657b2499fa9fea12df5d739c400218
Noey <noeyislearning@gmail.com>
2024-06-13 10:26:20 +0800

    refactor: update sidebar menu structure and add analytics section

--- docs/.vitepress/menus/sidebar/index.ts
+++ docs/.vitepress/menus/sidebar/index.ts
@@ -1,19 +1,32 @@
 /** Menus (Modularized) */
 import home from "./menus/home"
+
+/** Data Science  */
 import blueprintToADataScienceCareer from "./menus/lectures/data-science/blueprint-to-a-data-science-career"
+
+/** Analytics */
+import whatIsBusinessAnalysis from "./menus/lectures/analytics/what-is-business-analysis"
+
+/** Math */
 import linearAlgebra from "./menus/lectures/math/linear-algebra"
 import analyzingData from "./menus/lectures/programming/sql/analyzing-data"
+
+/** Programming */
 import introductionToSql from "./menus/lectures/programming/sql/introduction-to-sql"
 
 export default {
   "/": home,
+
   /** Data Science  */
   "/lectures/data-science/blueprint-to-a-data-science-career/": blueprintToADataScienceCareer,
 
+  /** Analytics */
+  "/lectures/analytics/what-is-business-analysis/": whatIsBusinessAnalysis,
+
   /** Math */
   "/lectures/math/linear-algebra/": linearAlgebra,
+  "/lectures/programming/sql/analyzing-data/": analyzingData,
 
   /** Programming */
-  "/lectures/programming/sql/analyzing-data/": analyzingData,
   "/lectures/programming/sql/introduction-to-sql": introductionToSql,
 }
