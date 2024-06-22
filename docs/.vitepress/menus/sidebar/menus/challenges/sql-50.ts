2aff98c38c29f77b178dbc48974b9fbbaa766780
Noey <noeyislearning@gmail.com>
2024-06-23 07:02:14 +0800

    refactor: (#40) update SQL 50 challenges menu structure

--- docs/.vitepress/menus/sidebar/menus/challenges/sql-50.ts
+++ docs/.vitepress/menus/sidebar/menus/challenges/sql-50.ts
@@ -1,42 +1,47 @@
 export default [
   {
     text: "1757. Recyclable and Low Fat Products",
-    link: "/challenges/sql-50/june-2024/1757-recyclable-and-low-fat-products",
+    link: "/challenges/sql-50/1757-recyclable-and-low-fat-products",
     difficulty: { level: "Easy" },
   },
   {
     text: "584. Find Customer Referee",
-    link: "/challenges/sql-50/june-2024/584-find-customer-referee",
+    link: "/challenges/sql-50/584-find-customer-referee",
     difficulty: { level: "Easy" },
   },
   {
     text: "595. Big Countries",
-    link: "/challenges/sql-50/june-2024/595-big-countries",
+    link: "/challenges/sql-50/595-big-countries",
     difficulty: { level: "Easy" },
   },
   {
     text: "570. Managers with at Least 5 Direct Reports",
-    link: "/challenges/sql-50/june-2024/570-managers-with-at-least-5-direct-reports",
+    link: "/challenges/sql-50/570-managers-with-at-least-5-direct-reports",
     difficulty: { level: "Medium" },
   },
   {
     text: "1148. Article Views I",
-    link: "/challenges/sql-50/june-2024/1148-article-views-i",
+    link: "/challenges/sql-50/1148-article-views-i",
     difficulty: { level: "Easy" },
   },
   {
     text: "1683. Invalid Tweets",
-    link: "/challenges/sql-50/june-2024/1683-invalid-tweets",
+    link: "/challenges/sql-50/1683-invalid-tweets",
     difficulty: { level: "Easy" },
   },
   {
     text: "185. Department Top Three Salaries",
-    link: "/challenges/sql-50/june-2024/185-department-top-three-salaries",
+    link: "/challenges/sql-50/185-department-top-three-salaries",
     difficulty: { level: "Hard" },
   },
   {
     text: "1378. Replace Employee ID With The Unique Identifier",
-    link: "/challenges/sql-50/june-2024/1378-replace-employee-id-with-the-unique-identifier",
+    link: "/challenges/sql-50/1378-replace-employee-id-with-the-unique-identifier",
+    difficulty: { level: "Easy" },
+  },
+  {
+    text: "1068. Product Sales Analysis I",
+    link: "/challenges/sql-50/1068-product-sales-analysis-i",
     difficulty: { level: "Easy" },
   },
 ]
