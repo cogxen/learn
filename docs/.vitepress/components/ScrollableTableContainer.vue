0e8b503530a2aab107143988cdc03472c6ee486b
Noey <noeyislearning@gmail.com>
2024-06-27 02:13:14 +0800

    style: (#39) update tables responsiveness

---
+++ docs/.vitepress/components/ScrollableTableContainer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="scrollable-table-container font-mono text-slate-500 dark:text-slate-300 font-light text-xs"
+  >
+    <slot />
+  </div>
+</template>
