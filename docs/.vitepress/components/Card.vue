c4c4ceaa4e14341edce36287f2f5142f1aaef7b8
Noey <noeyislearning@gmail.com>
2024-07-02 13:10:05 +0800

    style: add padding on `Card.vue`

--- docs/.vitepress/components/Card.vue
+++ docs/.vitepress/components/Card.vue
@@ -32,27 +32,22 @@ const trimmedUrl = computed(() => {
 </script>
 
 <template>
-  <div class="w-full overflow-hidden">
-    <div class="flex flex-row">
-      <img
-        class="h-40 w-40 md:w-48 lg:w-64 object-cover bg-emerald-500"
-        :src="img_url"
-        :alt="title"
-      />
-      <div
-        class="flex flex-col p-4 border-t border-r border-b border-slate-400 dark:border-slate-700 border-dashed"
-      >
-        <div class="flex flex-col gap-2 items-start">
-          <div class="uppercase tracking-wide text-sm text-emerald-500 font-semibold">
-            {{ title }}
-          </div>
-          <div class="text-slate-500 line-clamp-3 text-sm">{{ description }}</div>
+  <div class="w-full overflow-hidden mb-2">
+    <div class="flex flex-col p-4 h-36 border border-slate-400 dark:border-slate-700 border-dashed">
+      <div class="flex flex-col gap-2 items-start">
+        <div class="uppercase tracking-wide text-sm text-emerald-500 font-semibold">
+          {{ title }}
         </div>
-        <div class="text-xs flex flex-row gap-2 items-center mt-auto">
-          <Link2 class="w-4 h-4" />
-          <a :href="redirect_url" target="_blank">{{ trimmedUrl }}</a>
-        </div>
+        <div class="text-slate-500 line-clamp-2 text-sm">{{ description }}</div>
       </div>
+      <a
+        :href="redirect_url"
+        target="_blank"
+        class="text-xs flex flex-row gap-2 items-center mt-auto"
+      >
+        <Link2 class="w-4 h-4" />
+        <span>{{ trimmedUrl }}</span>
+      </a>
     </div>
   </div>
 </template>
