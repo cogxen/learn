3cd7de948a9a2b4f945668a5f38f1c005e523e56
Noey <noeyislearning@gmail.com>
2024-07-03 20:18:37 +0800

    feat: add metadata configuration for website

---
+++ config/constants.ts
@@ -0,0 +1,9 @@
+export const metadata = {
+  lang: "en-US",
+  locale: "en-US",
+  title: "Learn with Cogxen",
+  description:
+    "We empowers data enthusiasts and practitioners with the tools and knowledge to unlock the potential of data. We foster a collaborative environment for open-source projects, experimentation, and innovation in data science, machine learning, artificial intelligence, and related data careers.",
+  site: "https://learn.cogxen.quest",
+  image: "https://i.imgur.com/6FbwOkw.png",
+}
