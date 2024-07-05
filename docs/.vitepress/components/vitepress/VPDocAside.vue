96e58086713f8888f154b7fc18a3525054ee6b97
Noey <noeyislearning@gmail.com>
2024-07-06 01:15:32 +0800

    feat: add dataset download badge

--- docs/.vitepress/components/vitepress/VPDocAside.vue
+++ docs/.vitepress/components/vitepress/VPDocAside.vue
@@ -2,20 +2,18 @@
 import { onContentUpdated } from "vitepress"
 import { ref, type Ref } from "vue"
 import { useData } from "vitepress/dist/client/theme-default/composables/data.js"
+import type { Contributor, AssignedTo } from "../../../../types/doc-aside"
+import { Link2 } from "lucide-vue-next"
 
 // Components
 import VPDocAsideOutline from "./VPDocAsideOutline.vue"
 import VPDocAsideCarbonAds from "./VPDocAsideCarbonAds.vue"
 
-interface Contributor {
-  name: string
-  website_url: string
-}
-
 const { frontmatter, theme } = useData()
 const leetcodeStudyPlan = ref({ name: "", link: "", planImg: "" })
 const leetcodeInformation = ref({ number: "", problemName: "", link: "" })
 const contributors: Ref<Contributor[]> = ref([])
+const assignedTo: Ref<AssignedTo[]> = ref([])
 
 const updateleetcodeStudyPlan = () => {
   if (frontmatter.value.leetcodeStudyPlan !== undefined) {
@@ -62,9 +60,27 @@ const updateContributors = () => {
   }
 }
 
+const updateAssignedTo = () => {
+  if (frontmatter.value.assignedTo !== undefined) {
+    const assignedToDetails = frontmatter.value.assignedTo.map((detail: any) => {
+      return {
+        name: detail.name || "",
+        year: detail.year || "",
+        websiteURL: detail.websiteURL || "",
+        projectURL: detail.projectURL || "",
+      }
+    })
+
+    assignedTo.value = assignedToDetails
+  } else {
+    assignedTo.value = []
+  }
+}
+
 onContentUpdated(updateleetcodeStudyPlan)
 onContentUpdated(updateleetcodeInformation)
 onContentUpdated(updateContributors)
+onContentUpdated(updateAssignedTo)
 </script>
 
 <template>
@@ -115,8 +131,37 @@ onContentUpdated(updateContributors)
             target="_blank"
             class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden border border-dashed border-slate-300 dark:border-slate-700"
           >
-            <span class="font-medium text-gray-600 dark:text-gray-300">{{ contributor.name }}</span>
+            <span class="font-medium text-slate-600 dark:text-slate-300">{{
+              contributor.name
+            }}</span>
           </a>
+        </div>
+      </div>
+
+      <!-- AssignedTo -->
+      <div class="flex flex-col gap-2 items-start" v-if="assignedTo.length">
+        <span class="text-xs text-slate-800 dark:text-slate-300"> Asssigned to</span>
+        <div
+          v-for="item in assignedTo"
+          :key="item.name"
+          class="flex flex-col items-start gap-1 w-full p-2 border border-dashed border-slate-300 dark:border-slate-700"
+        >
+          <div class="font-medium text-sm text-slate-600 dark:text-slate-300">
+            {{ item.name }}
+            <span class="text-slate-400 dark:text-slate-500 text-xs">({{ item.year }} Year)</span>
+          </div>
+          <a
+            v-if="item.projectURL"
+            :href="item.projectURL"
+            target="_blank"
+            class="text-xs text-emerald-600 dark:text-emerald-300 flex flex-row gap-1 items-center"
+          >
+            <Link2 class="w-4 h-4" />
+            Project link</a
+          >
+          <div v-else class="text-xs text-emerald-400 dark:text-emerald-700">
+            Project link not available
+          </div>
         </div>
       </div>
     </slot>
