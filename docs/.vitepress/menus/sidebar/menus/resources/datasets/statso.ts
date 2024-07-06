d9bbb82330d7b9086897e2cf99c9dc2f7caa9757
Noey <noeyislearning@gmail.com>
2024-07-07 00:43:19 +0800

    refactor: move routings

---
+++ docs/.vitepress/menus/sidebar/menus/resources/datasets/statso.ts
@@ -0,0 +1,182 @@
+export default [
+  {
+    text: "Demand & Supply Analysis",
+    link: "/resources/datasets/statso/demand-and-supply-analysis",
+  },
+  {
+    text: "Recession Analysis",
+    link: "/resources/datasets/statso/recession-analysis",
+  },
+  {
+    text: "Job Recommendation System",
+    link: "/resources/datasets/statso/job-recommendation-system",
+  },
+  {
+    text: "Stock Market Anomaly Detection",
+    link: "/resources/datasets/statso/stock-market-anomaly-detection",
+  },
+  {
+    text: "Music Recommendation System Using Spotify API",
+    link: "/resources/datasets/statso/music-recommendation-system-using-spotify-api",
+  },
+  {
+    text: "Instagram Reach Analysis & Forecasting",
+    link: "/resources/datasets/statso/instagram-reach-analysis-and-forecasting",
+  },
+  {
+    text: "House Rent Prediction",
+    link: "/resources/datasets/statso/house-rent-prediction",
+  },
+  {
+    text: "IPL 2024 RCB vs DC Analysis",
+    link: "/resources/datasets/statso/ipl-2024-rcb-vs-dc-analysis",
+  },
+  {
+    text: "Book Recommendation System",
+    link: "/resources/datasets/statso/book-recommendation-system",
+  },
+  {
+    text: "Market Basket Analysis",
+    link: "/resources/datasets/statso/market-basket-analysis",
+  },
+  {
+    text: "Consumer Behaviour Analysis",
+    link: "/resources/datasets/statso/consumer-behaviour-analysis",
+  },
+  {
+    text: "Real Estate Price Prediction",
+    link: "/resources/datasets/statso/real-estate-price-prediction",
+  },
+  {
+    text: "Electric Vehicles Market Size Analysis",
+    link: "/resources/datasets/statso/electric-vehicles-market-size-analysis",
+  },
+  {
+    text: "App Reviews Sentiment Analysis",
+    link: "/resources/datasets/statso/app-reviews-sentiment-analysis",
+  },
+  {
+    text: "Car Insurance Modelling",
+    link: "/resources/datasets/statso/car-insurance-modelling",
+  },
+  {
+    text: "Spam Comments Detection",
+    link: "/resources/datasets/statso/spam-comments-detection",
+  },
+  {
+    text: "Loan Approval Prediction",
+    link: "/resources/datasets/statso/loan-approval-prediction",
+  },
+  {
+    text: "Retail Price Optimization",
+    link: "/resources/datasets/statso/retail-price-optimization",
+  },
+  {
+    text: "Food Delivery Cost and Profitability Analysis",
+    link: "/resources/datasets/statso/food-delivery-cost-and-profitablity-analysis",
+  },
+  {
+    text: "Delhi Metro Network Analysis",
+    link: "/resources/datasets/statso/delhi-metro-network-analysis",
+  },
+  {
+    text: "Demand Forecasting & Inventory Optimization",
+    link: "/resources/datasets/statso/demand-forecasting-and-inventory-optimization",
+  },
+  {
+    text: "Food Delivery Time Prediction",
+    link: "/resources/datasets/statso/food-delivery-time-prediction",
+  },
+  {
+    text: "Restaurant Recommendation System",
+    link: "/resources/datasets/statso/restaurant-recommendation-system",
+  },
+  {
+    text: "User Engagement Analysis",
+    link: "/resources/datasets/statso/user-engagement-analysis",
+  },
+  {
+    text: "Metro Operations Optimization",
+    link: "/resources/datasets/statso/metro-operations-optimization",
+  },
+  {
+    text: "Youtube Data Collection and Analysis",
+    link: "/resources/datasets/statso/youtube-data-collection-and-analysis",
+  },
+  {
+    text: "Website Performance Analysis",
+    link: "/resources/datasets/statso/website-performance-analysis",
+  },
+  {
+    text: "User Profiling & Segmentation",
+    link: "/resources/datasets/statso/user-profiling-and-segmentation",
+  },
+  {
+    text: "Quantitative Analysis of Stock Market",
+    link: "/resources/datasets/statso/quantitative-analysis-of-stock-market",
+  },
+  {
+    text: "Health Monitoring & Analysis",
+    link: "/resources/datasets/statso/health-monitoring-and-analysis",
+  },
+  {
+    text: "Stock Market Comparison Analysis",
+    link: "/resources/datasets/statso/stock-market-comparison-analysis",
+  },
+  {
+    text: "Netflix Subscriptions Forecasting",
+    link: "/resources/datasets/statso/netflix-subscriptions-forecasting",
+  },
+  {
+    text: "Credit Scoring & Segmentation",
+    link: "/resources/datasets/statso/credit-scoring-and-segmentation",
+  },
+  {
+    text: "Dynamic Pricing Strategy",
+    link: "/resources/datasets/statso/dynamic-pricing-strategy",
+  },
+  {
+    text: "Currency Exchange Rate Forecasting",
+    link: "/resources/datasets/statso/currency-exchange-rate-forecasting",
+  },
+  {
+    text: "Store Sales & Profit Analysis",
+    link: "/resources/datasets/statso/store-sales-and-profit-analysis",
+  },
+  {
+    text: "Weather Forecasting",
+    link: "/resources/datasets/statso/weather-forecasting",
+  },
+  {
+    text: "Screen Time Analysis",
+    link: "/resources/datasets/statso/screen-time-analysis",
+  },
+  {
+    text: "Business Forecasting",
+    link: "/resources/datasets/statso/business-forecasting",
+  },
+  {
+    text: "Salary Prediction",
+    link: "/resources/datasets/statso/salary-prediction",
+  },
+  {
+    text: "COVID-19 Spread and Impacts Analysis",
+    link: "/resources/datasets/statso/covid-19-spread-and-impacts-analysis",
+  },
+  {
+    text: "Student Marks Prediction",
+    link: "/resources/datasets/statso/student-marks-prediction",
+  },
+  {
+    text: "COVID-19 Deaths Prediction",
+    link: "/resources/datasets/statso/covid-19-deaths-prediction",
+  },
+  {
+    text: "Movie Rating Analysis",
+    link: "/resources/datasets/statso/movie-rating-analysis",
+  },
+  {
+    text: "Unemployment Analysis",
+    link: "/resources/datasets/statso/unemployment-analysis",
+  },
+]
