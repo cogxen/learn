0516a08a518539fd6935665b0fbad869bd866a8a
Noey <noeyislearning@gmail.com>
2024-07-11 01:16:51 +0800

    refactor: add new lectures, python for data science

--- docs/.vitepress/menus/sidebar/menus/lectures/programming/python/python-for-data-science.ts
+++ docs/.vitepress/menus/sidebar/menus/lectures/programming/python/python-for-data-science.ts
@@ -10,6 +10,34 @@ export default [
         text: "Importing Datasets",
         link: "/lectures/programming/python/python-for-data-science/exploring-data/importing-datasets",
       },
+      {
+        text: "Data Types",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/data-types",
+      },
+      {
+        text: "Variables",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/variables",
+      },
+      {
+        text: "Lists",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/lists",
+      },
+      {
+        text: "Dictionaries",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/dictionaries",
+      },
+      {
+        text: "Methods",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/methods",
+      },
+      {
+        text: "Categorical Data",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/categorical-data",
+      },
+      {
+        text: "Numeric Data",
+        link: "/lectures/programming/python/python-for-data-science/exploring-data/numeric-data",
+      },
     ],
   },
 ]
