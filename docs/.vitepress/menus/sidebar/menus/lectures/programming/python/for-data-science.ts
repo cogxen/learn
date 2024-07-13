73c76075e9f5568a9c4ed4bcd46928724641cada
Noey <noeyislearning@gmail.com>
2024-07-13 19:24:44 +0800

    refactor: update Python for Data Science lecture menu

---
+++ docs/.vitepress/menus/sidebar/menus/lectures/programming/python/for-data-science.ts
@@ -0,0 +1,92 @@
+export default [
+  {
+    text: "Exploring Data",
+    collapsed: true,
+    items: [
+      {
+        text: "Data Workspace",
+        link: "/lectures/programming/python/for-data-science/exploring-data/data-workspace",
+      },
+      {
+        text: "Importing Datasets",
+        link: "/lectures/programming/python/for-data-science/exploring-data/importing-datasets",
+      },
+      {
+        text: "Data Types",
+        link: "/lectures/programming/python/for-data-science/exploring-data/data-types",
+      },
+      {
+        text: "Variables",
+        link: "/lectures/programming/python/for-data-science/exploring-data/variables",
+      },
+      {
+        text: "Lists",
+        link: "/lectures/programming/python/for-data-science/exploring-data/lists",
+      },
+      {
+        text: "Dictionaries",
+        link: "/lectures/programming/python/for-data-science/exploring-data/dictionaries",
+      },
+      {
+        text: "Methods",
+        link: "/lectures/programming/python/for-data-science/exploring-data/methods",
+      },
+      {
+        text: "Categorical Data",
+        link: "/lectures/programming/python/for-data-science/exploring-data/categorical-data",
+      },
+      {
+        text: "Numeric Data",
+        link: "/lectures/programming/python/for-data-science/exploring-data/numeric-data",
+      },
+    ],
+  },
+  {
+    text: "Sorting and Filtering Data",
+    collapsed: true,
+    items: [
+      {
+        text: "Row Indexes",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/row-indexes",
+      },
+      {
+        text: "Sorting Rows",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/sorting-rows",
+      },
+      {
+        text: "Selecting Rows",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/selecting-rows",
+      },
+      {
+        text: "Selecting Ranges of Rows",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/selecting-ranges-of-rows",
+      },
+      {
+        text: "Boolean",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/boolean",
+      },
+      {
+        text: "Filtering Rows",
+        link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/filtering-rows",
+      },
+      {
+        text: "Operators",
+        collapsed: true,
+        items: [
+          {
+            text: "Booleans with And",
+            link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/operators/booleans-with-and",
+          },
+          {
+            text: "Booleans with Or",
+            link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/operators/booleans-with-or",
+          },
+          {
+            text: "Booleans with Not",
+            link: "/lectures/programming/python/for-data-science/sorting-and-filtering-data/operators/booleans-with-not",
+          },
+        ],
+      },
+    ],
+  },
+]
