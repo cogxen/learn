8bb6a9aef32b0f79aac0f2698f5a70ad4025f6a9
Noey <noeyislearning@gmail.com>
2024-07-15 07:33:11 +0800

    style: remove rounded css properties

--- docs/.vitepress/components/MathExampleCard.vue
+++ docs/.vitepress/components/MathExampleCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full h-full overflow-hidden relative">
     <div
-      class="p-6 text-center w-full h-fit border border-dashed border-slate-400 rounded-xl dark:border-slate-600"
+      class="p-6 text-center w-full h-fit border border-dashed border-slate-400 dark:border-slate-600"
     >
       <slot />
     </div>
-    <div class="absolute top-2 left-2 bg-slate-700 text-white text-[10px] px-2 py-1 rounded-lg">
+    <div class="absolute top-2 left-2 bg-slate-700 text-white text-[10px] px-2 py-1">
       {{ copyright_owner }}
     </div>
   </div>
