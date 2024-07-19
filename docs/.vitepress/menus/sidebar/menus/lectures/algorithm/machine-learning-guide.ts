0565571850e6be180668f3fb97ac1cc61219ad5c
Noey <noeyislearning@gmail.com>
2024-07-19 08:17:52 +0800

    feat: add linear regression algorithm to machine learning guide

--- docs/.vitepress/menus/sidebar/menus/lectures/algorithm/machine-learning-guide.ts
+++ docs/.vitepress/menus/sidebar/menus/lectures/algorithm/machine-learning-guide.ts
@@ -9,11 +9,67 @@ export default [
     ],
   },
   {
-    text: "Regression",
+    text: "Regression Algorithms",
     items: [
       {
         text: "Linear Regression",
-        link: "/lectures/algorithm/machine-learning-guide/regression/linear-regression",
+        collapsed: false,
+        items: [
+          {
+            text: "What is Linear Regression?",
+            link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/",
+          },
+          {
+            text: "Applications",
+            collapsed: false,
+            items: [
+              {
+                text: "Economics",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/applications/economics",
+              },
+              {
+                text: "Finance",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/applications/finance",
+              },
+              {
+                text: "Healthcare",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/applications/healthcare",
+              },
+              {
+                text: "Real Estate",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/applications/real-estate",
+              },
+            ],
+          },
+          {
+            text: "Advantages",
+            link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/advantages",
+          },
+          {
+            text: "Disadvantages",
+            link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/disadvantages",
+          },
+          {
+            text: "Examples",
+            collapsed: false,
+            items: [
+              {
+                text: "Simple",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/examples/simple",
+              },
+              {
+                text: "Multiple",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/examples/multiple",
+                badge: { text: "Working" },
+              },
+              {
+                text: "Polynomial",
+                link: "/lectures/algorithm/machine-learning-guide/regression-algorithms/linear-regression/examples/polynomial",
+                badge: { text: "Working" },
+              },
+            ],
+          },
+        ],
       },
     ],
   },
